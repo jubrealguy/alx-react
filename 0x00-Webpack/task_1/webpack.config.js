55ca4069fa0083a2f39ad04f7b012658008edeb4
jubrealguy <whizkidjubrealguy@gmail.com>
2023-10-24 05:36:05 +0100

    task 1

--- 0x00-Webpack/task_1/webpack.config.js
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -3,24 +3,10 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'development', // or 'production' based on your requirement
+  mode: 'production', // or 'production' based on your requirement
   entry: './js/dashboard_main.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader', // you can use appropriate loaders here
-          options: {
-            presets: ['@babel/preset-env'], // adjust based on your project's requirements
-          },
-        },
-      },
-    ],
+    path: path.resolve(__dirname, 'public'),
   },
 };
