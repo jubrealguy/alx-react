d19fd7ab726d12282ee3c74a89cd907153f6c686
jubrealguy <whizkidjubrealguy@gmail.com>
2023-11-07 13:00:15 +0100

    inline styling

---
+++ 0x04-React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const rowBackground = {
+    background: '#f5f5f5ab'
+}
+
+const headerBackground = {
+    background: '#deb5b545'
+}
+
+const CourseListRow = ({ isHeader, textFirstCell, textSecondCell }) => {
+    return (
+        <tr style={rowBackground}>
+            {isHeader ? 
+                (textSecondCell == null ? 
+                    (<th colSpan={2} style={headerBackground}>{textFirstCell}</th>)
+                    : (
+                        <>
+                            <th style={headerBackground}>{textFirstCell}</th>
+                            <th style={headerBackground}>{textSecondCell}</th>
+                        </>
+                    )) : (
+                        <>
+                            <td>{textFirstCell}</td>
+                            <td>{textSecondCell}</td>
+                        </>
+                    )}
+        </tr>
+    )
+}
+
+CourseListRow.PropTypes = {
+    isHeader: PropTypes.bool,
+    textFirstCell: PropTypes.string.isRequired,
+    textSecondCell: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
+}
+
+CourseListRow.defaultProps = {
+    isHeader: false,
+    textSecondCell: null,
+  };
+
+export default CourseListRow
