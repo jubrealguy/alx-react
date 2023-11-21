f3a7ef80318968f67077186561a0af0a41da6e39
jubrealguy <whizkidjubrealguy@gmail.com>
2023-11-21 13:11:46 +0100

    merge and concat

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -1,9 +1,9 @@
 import { List } from 'immutable';
 
 export default function concatElements(page1, page2) {
-    return List()
-}
+  return List(page1).concat(List(page2));
+};
 
 export default function mergeElements(page1, page2) {
-    
-}
+  Map(page1).merge(Map(page2));
+};
