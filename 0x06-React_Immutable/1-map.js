110f902848cfc4f4e3b29f263bc2b3c7293b7876
jubrealguy <whizkidjubrealguy@gmail.com>
2023-11-21 01:03:07 +0100

    Map

--- 0x06-React_Immutable/1-map.js
+++ 0x06-React_Immutable/1-map.js
@@ -1,5 +1,5 @@
 import { Map } from 'immutable';
 
-export default function getImmutableObject(object) {
-  Map(object)
-}
+const getImmutableObject = (object) => Map(object);
+
+export default getImmutableObject;
